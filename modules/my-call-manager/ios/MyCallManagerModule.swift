231ba9b720fb878e4625dd70a094c4ed325f1d9a
Alex Kwan <jalexzz@ymail.com>
2026-08-08 19:42:29 +0800

    fix build version

--- modules/my-call-manager/ios/MyCallManagerModule.swift
+++ modules/my-call-manager/ios/MyCallManagerModule.swift
@@ -4,38 +4,23 @@ import CallKit
 public class MyCallManagerModule: Module {
   public func definition() -> ModuleDefinition {
     Name("MyCallManager")
-    
-    // 1. Dedicated function to save Whitelist data to App Group
-    AsyncFunction("saveWhitelist") { (
-      prefixes: [Int64], 
-      specifics: [String: String], 
-      promise: Promise
-    ) in
-      if let sharedDefaults = UserDefaults(suiteName: "group.com.jalexzzStudio.hkCallBlocker") {
-        sharedDefaults.set(prefixes, forKey: "whitelistedPrefixes")
-        sharedDefaults.set(specifics, forKey: "specificWhitelist")
-        sharedDefaults.synchronize()
-        promise.resolve(true)
-      } else {
-        promise.reject("ERR_DEFAULTS", "Could not access App Group UserDefaults", nil)
-      }
-    }
 
     // Update the arguments to include startNumber and endNumber
     AsyncFunction("setBlockStateAndReload") { (isActive: Bool, startNumber: Int, endNumber: Int, identifier: String, promise: Promise) in
       
       // Save state to App Group
       if let sharedDefaults = UserDefaults(suiteName: "group.com.jalexzzStudio.hkCallBlocker") {
-        sharedDefaults.set(isActive, forKey: "isBlockActive_\(identifier)")
+        sharedDefaults.set(isActive, forKey: "isBlockActive")
         // Save the dynamic numbers
         sharedDefaults.set(startNumber, forKey: "startNumber")
         sharedDefaults.set(endNumber, forKey: "endNumber")
         sharedDefaults.synchronize()
       }
       
+      // Tell iOS CallKit to apply changes
       CXCallDirectoryManager.sharedInstance.reloadExtension(withIdentifier: identifier) { error in
         if let error = error {
-          promise.reject("ERR_RELOAD", error.localizedDescription, error)
+          promise.reject(error)
         } else {
           promise.resolve(true)
         }
