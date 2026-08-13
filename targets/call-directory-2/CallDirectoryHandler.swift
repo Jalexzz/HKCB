f7f229369e605b75198f5ca5dbf69b3361b97d68
Alex Kwan <jalexzz@ymail.com>
2026-08-14 01:03:11 +0800

    fix build

--- targets/call-directory-2/CallDirectoryHandler.swift
+++ targets/call-directory-2/CallDirectoryHandler.swift
@@ -12,7 +12,7 @@ class CallDirectoryHandler: CXCallDirectoryProvider {
         
         if isActive {
             // Pass the sharedDefaults so we can read the numbers
-            addAllBlockingPhoneNumbers(to: context, defaults: sharedDefaults)
+            addBatch2Numbers(to: context, defaults: sharedDefaults)
             print("Shield is ON. Numbers loaded.")
         } else {
             print("Shield is OFF. Block list cleared.")
@@ -21,7 +21,7 @@ class CallDirectoryHandler: CXCallDirectoryProvider {
         context.completeRequest()
     }
 
-    private func addAllBlockingPhoneNumbers(to context: CXCallDirectoryExtensionContext, defaults: UserDefaults?) {
+    private func addBatch2Numbers(to context: CXCallDirectoryExtensionContext, defaults: UserDefaults?) {
         // Retrieve the numbers from defaults, falling back to your original values if they don't exist
         let startNumber = defaults?.object(forKey: "startNumber") as? Int64 ?? 85230000000
         let endNumber = defaults?.object(forKey: "endNumber") as? Int64 ?? 85230000050
