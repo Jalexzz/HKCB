c5b8a8496a8bf40a21530cbdf9c3a8b33aa9c752
Alex Kwan <jalexzz@ymail.com>
2026-08-16 00:22:50 +0800

    test

--- targets/call-directory-two/expo-target.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
-module.exports = config => ({
-  type: "call-directory",
-  name: "Second Call Directory", // Must be unique
-  entitlements: { /* Add entitlements */ },
-});
