1abd87473e0d464e11364cb23cfa89c37a571bbe
Alex Kwan <jalexzz@ymail.com>
2026-08-16 00:55:23 +0800

    fix build

--- targets/FirstCallDirectory/expo-target.config.js
+++ targets/FirstCallDirectory/expo-target.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
 module.exports = config => ({
   type: "call-directory",
-  name: "First Call Directory", // Must be unique
+  name: "FirstCallDirectory", // Must be unique
   bundleId: "com.jalexzzStudio.hkCallBlocker.FirstCallDirectory",
   entitlements: {
     "com.apple.security.application-groups": [
