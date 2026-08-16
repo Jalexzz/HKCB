580782fa8627b00979cf398c2a68fe956d69c7ca
Alex Kwan <jalexzz@ymail.com>
2026-08-16 22:25:05 +0800

    test

--- targets/FirstCallDirectory/expo-target.config.js
+++ targets/FirstCallDirectory/expo-target.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
 module.exports = config => ({
   type: "call-directory",
+  deploymentTarget: "15.1",
   name: "FirstCallDirectory", // Must be unique
-  //bundleIdentifier: "com.jalexzzStudio.hkCallBlocker.FirstCallDirectory",
+  bundleIdentifier: ".FirstCallDirectory",
   entitlements: {
     "com.apple.security.application-groups": [
       "group.com.jalexzzStudio.hkCallBlocker"
--- targets/SecondCallDirectory/expo-target.config.js
+++ targets/SecondCallDirectory/expo-target.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
 module.exports = config => ({
   type: "call-directory",
+  deploymentTarget: "15.1",
   name: "SecondCallDirectory", // Must be unique
-  //bundleIdentifier: "com.jalexzzStudio.hkCallBlocker.SecondCallDirectory",
+  bundleIdentifier: ".SecondCallDirectory",
   entitlements: {
     "com.apple.security.application-groups": [
       "group.com.jalexzzStudio.hkCallBlocker"
