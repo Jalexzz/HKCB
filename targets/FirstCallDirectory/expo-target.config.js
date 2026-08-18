f0d2a87437ef9a60b7346c1147c8f87c41c109b9
Alex Kwan <jalexzz@ymail.com>
2026-08-18 23:38:51 +0800

    Bump version to 1.5.8 and add displayName to FirstCallDirectory config

--- targets/FirstCallDirectory/expo-target.config.js
+++ targets/FirstCallDirectory/expo-target.config.js
@@ -2,6 +2,7 @@ module.exports = {
   type: "call-directory",
   deploymentTarget: "16.4",
   name: "FirstCallDirectory",
+  displayName: "3 Blocker",
   bundleIdentifier: "com.jalexzzStudio.hkCallBlocker.FirstCallDirectory",
   entitlements: {
     "com.apple.security.application-groups": [
